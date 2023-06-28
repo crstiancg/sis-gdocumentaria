6f8bb7f0cd857d8389e3979c8ce8895333c29d14
Cristiancg <45461497+armindnwyr@users.noreply.github.com>
2023-06-27 20:37:53 -0500

    cambios en el buscador

--- app/Http/Livewire/Formtable.php
+++ app/Http/Livewire/Formtable.php
@@ -9,7 +9,7 @@ use Livewire\WithPagination;
 class Formtable extends Component
 {
     public $search="";
-    public $estado = "Revision";
+    // public $estado = "Revision";
     public $sortField;
     public $sortDirection = 'ASC';
     // use WithPagination;
@@ -23,24 +23,24 @@ class Formtable extends Component
     {
         return view('livewire.formtable',[
 
-            'noaceptado' => MesaAyuda::select('*')->where('estado',$this->estado)->search("asunto", $this->search)->paginate(10),
-            // 'aceptado' => MesaAyuda::select('*')->where('estado','Aceptado')->orderby('id','desc')->search("asunto", $this->search)->paginate(1),
+            'noaceptado' => MesaAyuda::select('*')->search("asunto", $this->search)->get(),
+            // 'aceptado' => MesaAyuda::select('*')->where('estado','Aceptado')->orderby('id','desc')->search("asunto", $this->search)->paginate(1) ---->where('estado',$this->estado),
             // 'revision' => MesaAyuda::select('*')->where('estado','Revision')->orderby('id','desc')->search("asunto", $this->search)->paginate(1),
 
         ]);
     }
 
-    public function revision($status)
-    {
-        $this->estado = $status;
-    }
-    public function aceptado($status)
-    {
-        $this->estado = $status;
-    }
-    public function noaceptado($status)
-    {
-        $this->estado = $status;
-    }
+    // public function revision($status)
+    // {
+    //     $this->estado = $status;
+    // }
+    // public function aceptado($status)
+    // {
+    //     $this->estado = $status;
+    // }
+    // public function noaceptado($status)
+    // {
+    //     $this->estado = $status;
+    // }
     
 }
